9d4ebacc4a26f2b7783e0002254582807d1deff1
Bojun Wang <bojunw2@uci.edu>
2018-06-14 16:17:08 -0700

    first commit

---
+++ RainApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.PSharp;
+
+
+namespace RainApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var configuration = Configuration.Create().WithVerbosityEnabled(2);
+            var runtime = PSharpRuntime.Create(configuration);
+            Program.Execute(runtime);
+            Console.WriteLine("Press Enter to terminate...");
+            Console.ReadLine();
+
+        }
+
+        [Microsoft.PSharp.Test]
+        public static void Execute(PSharpRuntime runtime)
+        {
+            runtime.CreateMachine(typeof(NetworkEnvironment));
+        }
+    }
+}
